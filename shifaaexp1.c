430e3ea506b58fc2a9fd1dfdef4dfb4363bce00b
shifaakhan20 <shifaakhan860@gmail.com>
2025-10-17 00:57:46 +0530

    Add files via upload

---
+++ shifaaexp1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    /*
+    Name: Shifaa 
+    age: 18
+    Class: comps 01
+    Division: C
+    UIN: 251P022
+    */
+
+    printf("Name: Shifaa \n");
+    printf("age: 18 \n");
+    printf("Class: comps 01 \n");
+    printf("Division: C \n");
+    printf("UIN: 251P022 \n");
+
+
+    return 0;
+}
