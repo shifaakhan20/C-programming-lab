844bdafedaf2824d3b54dfe8aa60f507102ae101
shifaakhan20 <shifaakhan860@gmail.com>
2025-11-30 17:27:35 +0530

    Add files via upload

---
+++ 10aexp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*Name: Shifaa
+  Roll No: 21
+  UIN: 251P022
+  Div: C
+*/
+
+int main() {
+    int num1, num2, sum;
+    int *ptr1, *ptr2;    
+
+    
+    ptr1 = &num1; 
+    ptr2 = &num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", ptr1); 
+
+    printf("Enter the second number: ");
+    scanf("%d", ptr2);
+
+    sum = *ptr1 + *ptr2;
+
+    printf("The sum of %d and %d is: %d\n", *ptr1, *ptr2, sum);
+
+    return 0;
+}
